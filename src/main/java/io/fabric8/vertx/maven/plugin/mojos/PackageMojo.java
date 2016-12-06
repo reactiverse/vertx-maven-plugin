2b76b2618d769d3f1bb5e71790360d422ae8276c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2016-12-06 14:11:26 +0530

    Copyright update

--- src/main/java/io/fabric8/vertx/maven/plugin/mojos/PackageMojo.java
+++ src/main/java/io/fabric8/vertx/maven/plugin/mojos/PackageMojo.java
@@ -1,17 +1,18 @@
 /*
- *   Copyright 2016 Kamesh Sampath
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *   Copyright (c) 2016 Red Hat, Inc.
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *   Red Hat licenses this file to you under the Apache License, version
+ *   2.0 (the "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *   implied.  See the License for the specific language governing
+ *   permissions and limitations under the License.
  */
 
 package io.fabric8.vertx.maven.plugin.mojos;
@@ -64,6 +65,27 @@ public class PackageMojo extends AbstractVertxMojo {
      */
     @Parameter(name = "classifier")
     protected String classifier;
+
+    public static String computeOutputName(MavenProject project, String classifier) {
+        String finalName = project.getBuild().getFinalName();
+        if (finalName != null) {
+            if (finalName.endsWith(".jar")) {
+                finalName = finalName.substring(0, finalName.length() - 4);
+            }
+            if (classifier != null && !classifier.isEmpty()) {
+                finalName += "-" + classifier;
+            }
+            finalName += ".jar";
+            return finalName;
+        } else {
+            finalName = project.getArtifactId() + "-" + project.getVersion();
+            if (classifier != null && !classifier.isEmpty()) {
+                finalName += "-" + classifier;
+            }
+            finalName += ".jar";
+            return finalName;
+        }
+    }
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -119,27 +141,6 @@ public class PackageMojo extends AbstractVertxMojo {
             throw new MojoFailureException("Unable to build fat jar", e);
         }
 
-    }
-
-    public static String computeOutputName(MavenProject project, String classifier) {
-        String finalName = project.getBuild().getFinalName();
-        if (finalName != null) {
-            if (finalName.endsWith(".jar")) {
-                finalName = finalName.substring(0, finalName.length() -4);
-            }
-            if (classifier != null  && ! classifier.isEmpty()) {
-                finalName += "-" + classifier;
-            }
-            finalName += ".jar";
-            return finalName;
-        } else {
-            finalName = project.getArtifactId() + "-" + project.getVersion();
-            if (classifier != null  && ! classifier.isEmpty()) {
-                finalName += "-" + classifier;
-            }
-            finalName += ".jar";
-            return finalName;
-        }
     }
 
 }
--- src/main/java/io/fabric8/vertx/maven/plugin/utils/FileUtils.java
+++ src/main/java/io/fabric8/vertx/maven/plugin/utils/FileUtils.java
@@ -1,3 +1,20 @@
+/*
+ *
+ *   Copyright (c) 2016 Red Hat, Inc.
+ *
+ *   Red Hat licenses this file to you under the Apache License, version
+ *   2.0 (the "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *   implied.  See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
 package io.fabric8.vertx.maven.plugin.utils;
 
 import java.io.*;
@@ -33,7 +50,7 @@ public class FileUtils {
         if (backupDir.exists() && backupDir.isDirectory()) {
 
             Files.move(sourceFilePath, Paths.get(backupDir.toString(), targetName)
-                    , StandardCopyOption.ATOMIC_MOVE);
+                , StandardCopyOption.ATOMIC_MOVE);
         }
 
         return backupFilePath;
@@ -58,7 +75,7 @@ public class FileUtils {
         if (destDir.exists() && destDir.isDirectory()) {
 
             Files.copy(sourceFilePath, Paths.get(destDir.toString(), targetName)
-                    , StandardCopyOption.REPLACE_EXISTING);
+                , StandardCopyOption.REPLACE_EXISTING);
         }
 
         return backupFilePath;
