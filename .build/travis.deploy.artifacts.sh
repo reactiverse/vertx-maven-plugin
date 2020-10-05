73df3ffb8dbb9a800467c3e8d342cd099a949da7
Thomas Segismont <tsegismo@redhat.com>
2020-10-05 13:40:16 +0200

    Fix Snapshot deployment

--- .build/travis.deploy.artifacts.sh
+++ .build/travis.deploy.artifacts.sh
@@ -1,4 +1,4 @@
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
 if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    mvn clean source:jar javadoc:jar deploy -s .travis.maven.settings.xml -B -DwithJavadoc -DskipTests
+    mvn clean source:jar javadoc:jar deploy -s travis.maven.settings.xml -B -DwithJavadoc -DskipTests
 fi
