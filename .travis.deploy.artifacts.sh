deadd821024c386a413e24fa63c8471d2fd606b3
Thomas Segismont <tsegismo@redhat.com>
2021-02-25 16:09:00 +0100

    Migrate to GH Actions
    Removed the coverage profile as we don't use it anyway.

--- .travis.deploy.artifacts.sh
+++
@@ -1,4 +0,0 @@
-PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
-if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
-    mvn clean source:jar deploy -s .travis.maven.settings.xml -B -DskipTests
-fi
