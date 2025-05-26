7106ba834246caf1d1bc2494cfd08d6736093a04
Thomas Segismont <tsegismo@redhat.com>
2025-05-26 17:56:38 +0200

    Fixes for RedeployIT

--- src/test/resources/projects/redeploy-with-extended-launcher-it/src/main/java/demo/Main.java
+++ src/test/resources/projects/redeploy-with-extended-launcher-it/src/main/java/demo/Main.java
@@ -1,23 +1,32 @@
 package demo;
 
 import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Launcher;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.launcher.application.*;
 
 /**
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
-public class Main extends Launcher {
+public class Main extends VertxApplication implements VertxApplicationHooks {
 
     public static void main(String[] args) {
-        new Main().dispatch(args);
+        Main app = new Main(args);
+        app.launch();
+    }
+
+    public Main(String[] args) {
+        super(args);
     }
 
     @Override
-    public void beforeDeployingVerticle(DeploymentOptions deploymentOptions) {
+    public void beforeDeployingVerticle(HookContext context) {
         String prefix = "Buongiorno";
-        deploymentOptions.setConfig(new JsonObject().put("prefix", prefix));
+        context.deploymentOptions().setConfig(new JsonObject().put("prefix", prefix));
     }
 
+    @Override
+    public void afterVerticleDeployed(HookContext context) {
+        System.out.println("Hooray!");
+    }
 }
